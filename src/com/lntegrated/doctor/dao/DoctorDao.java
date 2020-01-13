66242360f4505b759d31f1ec6a795aa55f1b9088
spartan2395 <dbtndhkd00@gmail.com>
2020-01-13 19:17:46 +0900

    doctor/member/clinic 완료

--- src/com/lntegrated/doctor/dao/DoctorDao.java
+++ src/com/lntegrated/doctor/dao/DoctorDao.java
@@ -1,6 +1,89 @@
 package com.lntegrated.doctor.dao;
 
-public class DoctorDao {
+import java.util.List;
+
+import javax.websocket.Session;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.lntegrated.doctor.SqlConfig;
+import com.lntegrated.doctor.dto.DoctorDto;
+
+public class DoctorDao extends SqlConfig{
 	String namespace = "com.lntegerated.doctor_mapper";
-	
+	SqlSession session = null;
+	//????
+	public List<DoctorDto> doctorList(){
+		List<DoctorDto> list = null;
+		try {
+			session = getSessionFactory().openSession();
+			list = session.selectList(namespace+"doctor_list");
+		}catch(Exception e) {
+			System.out.println("Doctor_list ERROR");
+		}finally {
+			session.close();
+		}
+		return list;
+	}
+	//회원 정보 검색
+	public DoctorDto doctorInfo(String id_d) {
+		DoctorDto dto = null;
+		try {
+			session = getSessionFactory().openSession();
+			dto = session.selectOne(namespace+"doctor_info", id_d);
+		}catch(Exception e) {
+			System.out.println("Doctor_info ERROR");
+		}finally {
+			session.close();
+		}
+		return dto;
+	}
+	//회원 가입
+	public int insert(DoctorDto dto) {
+		int res = 0;
+		try {
+			session = getSessionFactory().openSession();
+			res = session.insert(namespace+"doctor_insert", dto);
+			if(res > 0) {
+				session.commit();
+			}
+		}catch(Exception e) {
+			System.out.println("Doctor_insert ERROR");
+		}finally {
+			session.close();
+		}
+		return res;
+	}
+	//회원 정보 수정
+	public int update(DoctorDto dto) {
+		int res = 0;
+		try {
+			session = getSessionFactory().openSession();
+			res = session.update(namespace+"doctor_update", dto);
+			if(res > 0) {
+				session.commit();
+			}
+		}catch(Exception e) {
+			System.out.println("Doctor_update ERROR");
+		}finally {
+			session.close();
+		}
+		return res;
+	}
+	//회원 삭제
+	public int delete(String id_d) {
+		int res = 0;
+		try {
+			session = getSessionFactory().openSession();
+			res = session.update(namespace+"doctor_delete", id_d);
+			if(res > 0) {
+				session.commit();
+			}
+		}catch(Exception e) {
+			System.out.println("Doctor_delete ERROR");
+		}finally {
+			session.close();
+		}
+		return res;
+	}
 }
