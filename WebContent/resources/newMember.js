09cea5e9fbb138336b1035af9ffea2beedaf3150
Jiae-M <jiae95.min@gmail.com>
2020-01-14 15:19:33 +0900

    회원 가입 폼 js 포

---
+++ WebContent/resources/newMember.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+    var arraylist = document.getElementsByTagName("input");
+
+    for (var i = 0; i < arraylist.length; i++) {
+        arraylist[i].addEventListener("focusin", function () {
+            this.parentElement.style.border = "1px inset #daeff7";
+            if(this.value==""){
+                this.parentElement.parentElement.lastElementChild.innerHTML="필수 요소입니다."
+            } else {
+                this.parentElement.parentElement.lastElementChild.innerHTML=""
+            }
+        })
+        arraylist[i].addEventListener("focusout", function () {
+            this.parentElement.style.border = "solid 1px #75adc7";
+            if(this.value==""){
+                this.parentElement.parentElement.lastElementChild.innerHTML="필수 요소입니다."
+            } else {
+                this.parentElement.parentElement.lastElementChild.innerHTML=""
+            }
+        })
+    }
+
+    for (var i = 0; i < arraylist.length; i++) {
+        if (arraylist[i].value !=null){
+            
+        }
+    }
+
+
+};
